abb220ff7ce9ed2e53ac9ec55e6538532ff61910
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2024-06-29 19:32:36 +0530

    Create index.py

---
+++ Rotate Array/index.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        k = k % len(nums)      
+       
+        r = len(nums) - k
+      
+        new = nums[0:r]
+       
+        nums[0:r] = []
+        nums.extend(new)
+
+        """
+        Do not return anything, modify nums in-place instead.
+        """
