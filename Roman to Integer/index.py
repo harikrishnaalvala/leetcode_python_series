1bfa92b22bf01c4f3d94ea735c89f8786dbd911c
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2024-07-01 11:20:24 +0530

    Create index.py

---
+++ Roman to Integer/index.py
@@ -0,0 +1,20 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_nums= {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        integer_value = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            integer_value += roman_nums[char]
+        return integer_value
+
+
