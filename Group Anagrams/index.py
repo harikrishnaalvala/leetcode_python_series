c1179ab0487f386b1b4f2fa5a1032a874d91f1f9
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2024-07-03 19:35:53 +0530

    Create index.py

---
+++ Group Anagrams/index.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        grouped = {}
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            if sorted_word in grouped:
+                grouped[sorted_word].append(word)
+            else:
+                grouped[sorted_word] = [word]
+        return list(grouped.values())
